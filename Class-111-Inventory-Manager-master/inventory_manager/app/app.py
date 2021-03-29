c8d0c3942397f8f4729517f83b589b68302bac77
DeAndreFuentes <74945613+DeAndreFuentes@users.noreply.github.com>
2021-03-30 03:23:35 +0500

    fixes

--- Class-111-Inventory-Manager-master/inventory_manager/app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap
-
-app = Flask(__name__)
-app.secret_key = 'super secret key'
-Bootstrap(app)
-from inventory_manager.app.routes import routes
-
